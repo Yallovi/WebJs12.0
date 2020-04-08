6867fcf633f31e07b249839d1833b80b1d941395
Yallovi <vivimoskalenko@yandex.ru>
2020-04-09 00:44:30 +0300

    Lesson 09 Доступ к элементам на странице. DOM

---
+++ js/script.js
@@ -0,0 +1,62 @@
+let button = document.getElementById('start');
+console.log('button: ', button);
+
+let butnPlusIncome = document.getElementsByTagName('button')[0];
+console.log('butnPlusIncome: ', butnPlusIncome);
+
+let butnPlusExpenses = document.getElementsByTagName('button')[1];
+console.log('butnPlusExpenses: ', butnPlusExpenses);
+
+let depCheck = document.querySelector('#deposit-check');
+console.log('depCheck: ', depCheck);
+
+let additionalIncome = document.querySelectorAll('.additional_income-item');
+console.log('additionalIncome: ', additionalIncome);
+
+let budgetDay = document.getElementsByClassName('budget_day-value');
+console.log('budgetDay: ', budgetDay);
+
+let expensesMonth = document.getElementsByClassName('result-total expenses_month-value');
+console.log('expensesMonth: ', expensesMonth);
+
+let additionalIncomeTwo = document.getElementsByClassName('result-total additional_income-value');
+console.log('additionalIncomeTwo: ', additionalIncomeTwo);
+
+let additionalExpensesy = document.getElementsByClassName('result-total additional_expenses-value');
+console.log('additionalExpensesy: ', additionalExpensesy);
+
+let incomePeriod = document.getElementsByClassName('result-total income_period-value');
+console.log('incomePeriod: ', incomePeriod);
+
+let targetMonth = document.getElementsByClassName('result-total target_month-value');
+console.log('targetMonth: ', targetMonth);
+
+let salaryAmount = document.querySelector('.salary-amount');
+console.log('salaryAmount: ', salaryAmount);
+
+let incomeTitle = document.querySelector('.income-title');
+console.log('incomeTitle: ', incomeTitle);
+
+let incomeAmount = document.querySelector('.income-amount');
+console.log('incomeAmount: ', incomeAmount);
+
+let additionalIncomethree = document.querySelectorAll('.additional_income-item');
+console.log('additionalIncomethree: ', additionalIncomethree);
+
+let expensesTitle = document.querySelector('.expenses-title');
+console.log('expensesTitle: ', expensesTitle);
+
+let expensesAmount = document.querySelector('.expenses-amount');
+console.log('expensesAmount: ', expensesAmount);
+
+let additionalExpenses = document.querySelector('.additional_expenses-item');
+console.log('additionalExpenses: ', additionalExpenses);
+
+let depositCheck = document.querySelector('#deposit-check');
+console.log('depositCheck: ', depositCheck);
+
+let targetAmount = document.querySelector('.target-amount');
+console.log('targetAmount: ', targetAmount);
+
+let periodSelect = document.querySelector('[type = "range"]');
+console.log('periodSelect: ', periodSelect);

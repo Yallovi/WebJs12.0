c846da5be04e25fb8cf456ca1ea8a61391d26242
Yallovi <vivimoskalenko@yandex.ru>
2020-04-20 23:23:13 +0300

    to do list is ready

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,80 @@
 'use strict';
 
-const todoСontrol = document.querySelector('.todo-control'),
+
+const todoControl = document.querySelector('.todo-control'),
     headerInput = document.querySelector('.header-input'),
     todoList = document.querySelector('.todo-list'),
     todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = [];
+let todoData = [];
 
-const showText = function(){
-    todoData.textContent = localStorage.myText;
+
+for(let i = 0; i<localStorage.length; i++){
+    let key = localStorage.key(i);
+    let compl = localStorage.getItem(key) === "true" ? true : false;
+    let newTodo = {
+        value: key,
+        completed: compl
+    };
+    todoData.push(newTodo);
 }
 
-todoСontrol.addEventListener('submit', function(){
-    localStorage.myText = headerInput.value; 
-    showText(); 
-});
+const render = function() {
 
-const render = function(){
     todoList.textContent = '';
     todoCompleted.textContent = '';
 
     todoData.forEach(function(item){
-    const li = document.createElement('li');
-    li.classList.add('todo-item');  
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
 
-    li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
-        '<div class="todo-buttons">' +
-            '<button class="todo-remove"></button>' +
-            '<button class="todo-complete"></button>' +
-            '</div>';   
-
-            if(item.completed){
-                todoCompleted.append(li)
-            } else {
-                todoList.append(li);
-            }
+        li.innerHTML = '<span class="text-todo">'+item.value+'</span>'+
+                    '<div class="todo-buttons">'+
+                        '<button class="todo-remove"></button>'+
+                        '<button class="todo-complete"></button>'+
+                    '</div>';
+        console.log(item);
+        if(item.completed){
+            todoCompleted.append(li);
+        } else{
+            todoList.append(li);
+        }
         
-        const btnTodoComplete = li.querySelector('.todo-complete')
-       
-        btnTodoComplete.addEventListener('click', function(){
-            item.completed = !item.completed;
-        render();
-        });
-        const btnTodoRemove = li.querySelector('.todo-remove');
-        btnTodoRemove.addEventListener('click', function(){
-            li.remove(item);
-        });
+        const btntodoComplete = li.querySelector('.todo-complete');
+        const btntodoRemove = li.querySelector('.todo-remove');
 
-    });
+        btntodoComplete.addEventListener('click', function(){
+            item.completed = !item.completed;
+            let localComplete = localStorage.getItem(item.value) === "true" ? true : false;
+            
+            localStorage.setItem(item.value,(!localComplete));
+            console.log((!localComplete));
+            render();
+        });
+        btntodoRemove.addEventListener('click', function(){
+            let index = todoData.indexOf(item);
+            todoData.splice(index,1);
+            localStorage.removeItem(item.value);
+            render();
+        });
+});
+
 };
 
-todoСontrol.addEventListener('submit', function(event){
-    if(headerInput.value === ''){
-        todoControl.disabled = true;
-    }else {
+todoControl.addEventListener("submit", function(event){
     event.preventDefault();
-
+    if(!headerInput.value){ 
+    return;}
     const newTodo = {
-        
-        value: headerInput.value,
-        completed : false
-        
+        value: headerInput.value ,
+        completed: false
     };
 
     todoData.push(newTodo);
+    localStorage.setItem(newTodo.value,false);
     headerInput.value = '';
 
     render();
-    }
 });
-showText();
+
 render();
-
-// showText = function(){
-//     todoList.textContent = localStorage.getItem('memory');
-// };
-
-// todoСontrol.addEventListener('submit', function(){
-//     localStorage.setItem('memory', headerInput.value);
-//     showText();
-// });

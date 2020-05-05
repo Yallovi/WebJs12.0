f11c9f1295366247a2085822c50de169d1c87b2e
Yallovi <vivimoskalenko@yandex.ru>
2020-05-06 01:12:46 +0300

    lesson26 Promise

--- script/main.js
+++ script/main.js
@@ -326,5 +326,81 @@ window.addEventListener('DOMContentLoaded', () => {
 
 calc();
 
+    //send-ajax-form
+
+    const sendForm = () => {
+        const errorMessage = 'Что то пошло не так...',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+        
+        const forms = document.forms;
+
+        const statusMessage = document.createElement('div');
+        statusMessage.textContent = 'Тут будет сообщение!';
+        statusMessage.style.cssText = 'font-size: 2rem; color: white;';
+
+        const postData = body => new Promise((resolve, reject) => {
+
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                console.log(request.readyState);
+                if (request.readyState !== 4) {
+                    return;
+                }
+
+                if (request.status === 200) {
+                    resolve();
+                } else {
+                    reject(request.status);
+                }
+                    
+                    
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+                
+
+            request.send(JSON.stringify(body));
+
+        });
+
+        [...forms].forEach(form => {
+
+            form.addEventListener('submit', event => {
+                
+                event.preventDefault();
+                form.appendChild(statusMessage);
+                statusMessage.textContent = loadMessage;
+                
+                const formData = new FormData(form);
+                let body = {};
+
+                formData.forEach((val, key) => {
+                    body[key] = val;
+                });
+
+                postData(body)
+                    .then(() => statusMessage.textContent = successMessage)
+                    .catch(error => {
+                        console.log(error);
+                        statusMessage.textContent = errorMessage;
+                    });
+
+                [...form.elements].forEach(elem => {
+                    if (elem.tagName.toLowerCase() === 'input') {
+                        elem.value = '';
+                    }
+                });
+            });
+        });
+        
+
+    };
+
+    sendForm();
+
+
+
 });
 
